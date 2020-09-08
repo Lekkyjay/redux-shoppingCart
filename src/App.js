18095282778db0cb5178498d285bcc8addbb66c9
Lekkyjay <lekkyjayjay@yahoo.com>
2020-09-08 13:02:52 +0200

    Implemented product filter, sort, and addToCart functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data.json'
+import Products from './components/Products'
+import Filter from './components/Filter'
+import Cart from './components/Cart'
 
 function App() {
+  const [item, setItem] = useState({
+    products: data.products,
+    cartItems: localStorage.getItem("cartItems") ? 
+      JSON.parse(localStorage.getItem("cartItems")) : [],
+    size: "",
+    sort: ""
+  })
+
+  const createOrder = (order) => {
+    alert("Need to save order for " + order.name);
+  };
+
+  const removeFromCart = (product) => {
+    const cartItems = item.cartItems.slice();
+    setItem({ ...item,
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+    localStorage.setItem("cartItems", 
+      JSON.stringify(item.cartItems.filter((x) => x._id !== product._id))
+    );
+  };
+
+  const addToCart = (product) => {
+    const cartItems = item.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((cartItem) => {
+      if (cartItem._id === product._id) {
+        cartItem.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    setItem({ ...item, cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
+
+  const sortProducts = (event) => {
+    const sort = event.target.value;
+    console.log('sort', event.target.value);
+    setItem({...item,
+      sort: sort,
+      products: item.products.slice().sort((a, b) =>
+          sort === "lowest" ? 
+            a.price > b.price ? 1 : -1
+          : sort === "highest" ? 
+            a.price < b.price ? 1 : -1
+          : a._id < b._id ? 1 : -1
+        ),
+    });
+  }
+
+  const filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      setItem({...item, size: event.target.value, products: data.products });
+    } else {
+      setItem({...item,
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Filter
+              count={item.products.length}
+              size={item.size}
+              sort={item.sort}
+              filterProducts={filterProducts}
+              sortProducts={sortProducts}
+            ></Filter>
+            <Products
+              products={item.products}
+              addToCart={addToCart}
+            ></Products>
+          </div>
+          <div className="sidebar">
+            <Cart
+              cartItems={item.cartItems}
+              removeFromCart={removeFromCart}
+              createOrder={createOrder}
+            />
+          </div>
+        </div>
+      </main>
+      <footer>All right is reserved.</footer>
     </div>
   );
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
+
+const  Cart = ({ cartItems, removeFromCart, createOrder }) => {
+
+  const [client, setClient] = useState({
+    name: "",
+    email: "",
+    address: "",
+    showCheckout: false,
+  })
+
+
+  const handleInput = (e) => {
+    setClient({...client, [e.target.name]: e.target.value });
+  };
+
+  const handleCheckout = (e) => {
+    e.preventDefault();
+    const order = {
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      cartItems: cartItems,
+    };
+    createOrder(order);
+  };
+  
+  return (
+    <div>
+      {cartItems.length === 0 ? (
+        <div className="cart cart-header">Cart is empty</div>
+      ) : (
+        <div className="cart cart-header">
+          You have {cartItems.length} in the cart{" "}
+        </div>
+      )}
+      <div>
+        <div className="cart">
+          <Fade left cascade>
+            <ul className="cart-items">
+              {cartItems.map((item) => (
+                <li key={item._id}>
+                  <div>
+                    <img src={item.image} alt={item.title}></img>
+                  </div>
+                  <div>
+                    <div>{item.title}</div>
+                    <div className="right">
+                      {formatCurrency(item.price)} x {item.count}{" "}
+                      <button
+                        className="button"
+                        onClick={() => removeFromCart(item)} 
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </Fade>
+        </div>
+        {cartItems.length !== 0 && (
+          <div>
+            <div className="cart">
+              <div className="total">
+                <div>
+                  Total:{" "}
+                  {formatCurrency(
+                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                  )}
+                </div>
+                <button
+                  onClick={ () => setClient({...client, showCheckout: true }) }
+                  className="button primary"
+                >
+                  Proceed
+                </button>
+              </div>
+            </div>
+            {client.showCheckout && (
+              <Fade right cascade>
+                <div className="cart">
+                  <form onSubmit={handleCheckout}>
+                    <ul className="form-container">
+                      <li>
+                        <label>Email</label>
+                        <input
+                          name="email"
+                          type="email"
+                          required
+                          onChange={handleInput}    
+                        ></input>
+                      </li>
+                      <li>
+                        <label>Name</label>
+                        <input
+                          name="name"
+                          type="text"
+                          required
+                          onChange={handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <label>Address</label>
+                        <input
+                          name="address"
+                          type="text"
+                          required
+                          onChange={handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <button className="button primary" type="submit"> Checkout </button>
+                      </li>
+                    </ul>
+                  </form>
+                </div>
+              </Fade>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+
+export default Cart;
