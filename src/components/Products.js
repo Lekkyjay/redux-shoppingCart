a48e4ce665a9a0c3b57742da3be7694203240880
Lekkyjay <lekkyjayjay@yahoo.com>
2020-09-08 19:41:56 +0200

    Successfully fetched products from the backend api

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import formatCurrency from "../util";
 import Fade from "react-reveal/Fade";
 import Modal from "react-modal";
 import Zoom from "react-reveal/Zoom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../actions/productActions"
 
-const Products = ({ products, addToCart }) => {
+const Products = ({ addToCart }) => {
+
   const [product, setProduct] = useState(null)
+  const dispatch = useDispatch()
+  const products = useSelector(state => state.products.items)
+  
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
 
   const openModal = (product) => {
     setProduct(product);
@@ -19,24 +28,27 @@ const Products = ({ products, addToCart }) => {
   return (
     <div>
       <Fade bottom cascade>
-        <ul className="products">
-          {products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id} onClick={() => openModal(product)} >
-                  <img src={product.image} alt={product.title}></img>
-                  <p>{product.title}</p>
-                </a>
-                <div className="product-price">
-                  <div>{formatCurrency(product.price)}</div>
-                  <button onClick={() => addToCart(product)} className="button primary" >
-                    Add To Cart
-                  </button>
+        {!products ? (<div>Loading ...</div>) : (
+          <ul className="products">
+            {products.map((product) => (
+              <li key={product._id}>
+                <div className="product">
+                  <a href={"#" + product._id} onClick={() => openModal(product)} >
+                    <img src={product.image} alt={product.title}></img>
+                    <p>{product.title}</p>
+                  </a>
+                  <div className="product-price">
+                    <div>{formatCurrency(product.price)}</div>
+                    <button onClick={() => addToCart(product)} className="button primary" >
+                      Add To Cart
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        )}
+        
       </Fade>
       {product && (
         <Modal isOpen={true} onRequestClose={closeModal}>
