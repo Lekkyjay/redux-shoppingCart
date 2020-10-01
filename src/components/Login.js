4461ca5a60879d1c41084233f8a0a56edaf4725c
Lekkyjay <lekkyjayjay@yahoo.com>
2020-10-01 15:59:06 +0200

    Built the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
 
 const Login = () => {
+  
+  const loginUser = () => {
+    Axios({
+      method: "POST",
+      data: { email, password},
+      withCredentials: true,
+      url: "http://localhost:5000/login",
+    }).then((res) => console.log(res));
+  };
+
   return (
-    <div>
-      login
+    <div className="login">
+      <h1>Login</h1>
+      <input placeholder="username" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={loginUser}>Login</button>
     </div>
   )
 }
